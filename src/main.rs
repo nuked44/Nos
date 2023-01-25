ca8e4ea0c1eb3134731e5c55b7a47f3648286509
nuked44 <nuked44@gmail.com>
2023-01-25 12:07:51 +0100

    custom architecture implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+<<<<<<< HEAD
 fn main() {
     println!("Hello, world!");
+=======
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+>>>>>>> 2931023 (custom architecture implemented)
 }
